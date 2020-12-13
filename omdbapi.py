37ff0877afbf148fc251ce2c8531e1a3e7b7b331
laknath123 <laknathdias@gmail.com>
2020-12-12 19:33:53 -0600

    added new functions
    Completed the functions for getting the movie plot and movie poster

--- omdbapi.py
+++ omdbapi.py
@@ -4,15 +4,46 @@ Created on Sat Nov 28 12:25:07 2020
 
 @author: lakna
 """
+# Required Library's
 import json
-import pandas as pd
 import requests
+import urllib
+import os
+# Change the path to the directory you want posters to be saved to
+#os.chdir('C:\\Users\\lakna\\OneDrive\\Desktop\\Early-Birds-MovieScraping')
 
-x = {'Content-Type': 'application/json'}
-url ='http://www.omdbapi.com/?i=tt3896198&apikey=9fc8c547'
-response = requests.get(url, headers = x)
-if response.status_code == 200:
-    thedict = json.loads(response.content.decode('utf-8'))
-else:
-    "response failed"
+
+#User input function
+# The user input function has to return
+
+
+
+
+def movie_plot(ID):
+    x = {'Content-Type': 'application/json'}
+    url ='http://www.omdbapi.com/?i='+ID+'&apikey=9fc8c547'
+    response = requests.get(url, headers = x)
+    if response.status_code == 200:
+        data = json.loads(response.content.decode('utf-8'))
+        print('The Plot for',data['Title'])
+        print(data['Plot'])
+    else:
+        "response failed"
+        
+def movie_poster(ID):
+    x = {'Content-Type': 'application/json'}
+    url ='http://www.omdbapi.com/?i='+ID+'&apikey=9fc8c547'
+    response = requests.get(url, headers = x)
+    if response.status_code == 200:
+        data = json.loads(response.content.decode('utf-8'))
+        image_url=data['Poster']
+        title=data['Title']
+        urllib.request.urlretrieve(image_url, title+'.jpg')
+    else:
+        "response failed"
+        
+        
     
+movie_plot('tt0386676')  # This is just a test id to check if the Id is working
+   
+movie_plot('tt4955642')
